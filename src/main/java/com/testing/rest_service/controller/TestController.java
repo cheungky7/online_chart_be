fc33b08ce24af7599172a60b7e4ac1a700f2229d
king yin cheung <kycheung1985@gmail.com>
2024-07-07 12:12:29 +0800

    init

---
+++ src/main/java/com/testing/rest_service/controller/TestController.java
@@ -0,0 +1,20 @@
+package com.testing.rest_service.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("testing")
+public class TestController {
+
+    @GetMapping("/number/{id}")
+    String testNumber(@PathVariable Long id){
+        String ret="Return number:"+id;
+
+        return ret;
+    }
+
+}
